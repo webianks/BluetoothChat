1d297ea9b997f3be663e0630b99d944a1659d7d3
webianks <webianks@gmail.com>
2017-07-14 22:43:02 +0530

    Bluetooth enable logic.

--- app/src/main/java/com/webianks/bluechat/MainActivity.kt
+++ app/src/main/java/com/webianks/bluechat/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.webianks.bluechat
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_ENABLE_BT: Int = 123
+    private lateinit var label: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        label = findViewById(R.id.label)
+
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(mBluetoothAdapter == null){
+          //This device doesn't support bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK)
+            label.setText("Bluetooth is now enabled.")
     }
 }
