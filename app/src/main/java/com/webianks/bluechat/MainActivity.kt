00256a71fc04d7009922911ac92875400e06201d
webianks <webianks@gmail.com>
2017-07-15 12:51:01 +0530

    Getting already paired devices + getting new devices.

--- app/src/main/java/com/webianks/bluechat/MainActivity.kt
+++ app/src/main/java/com/webianks/bluechat/MainActivity.kt
@@ -2,15 +2,24 @@ package com.webianks.bluechat
 
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.content.BroadcastReceiver
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_ENABLE_BT: Int = 123
     private lateinit var label: TextView
+    private val TAG: String = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +36,49 @@ class MainActivity : AppCompatActivity() {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
+
+        val pairedDevices = mBluetoothAdapter.bondedDevices
+
+        if (pairedDevices.size > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (device in pairedDevices) {
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+
+                Log.d(TAG,"deviceName: $deviceName :: deviceHardwareAddress: $deviceHardwareAddress")
+            }
+        }
+
+        // Register for broadcasts when a device is discovered.
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mReceiver, filter)
+
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val mReceiver = object: BroadcastReceiver() {
+        override fun onReceive(context : Context,intent : Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+            }
+        }
+    };
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_OK)
             label.setText("Bluetooth is now enabled.")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(mReceiver)
     }
 }
