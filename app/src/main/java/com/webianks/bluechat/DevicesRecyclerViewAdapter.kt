4321dba47a5d0aabeec21389589074b79687ad98
webianks <webianks@gmail.com>
2017-07-18 23:46:07 +0530

    recyclerview logic.

--- app/src/main/java/com/webianks/bluechat/DevicesRecyclerViewAdapter.kt
+++ app/src/main/java/com/webianks/bluechat/DevicesRecyclerViewAdapter.kt
@@ -1,29 +1,34 @@
 package com.webianks.bluechat
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 /**
  * Created by R Ankit on 18-07-2017.
  */
 
-class DevicesRecyclerViewAdapter : RecyclerView.Adapter<DevicesRecyclerViewAdapter.VH>(){
+class DevicesRecyclerViewAdapter(val mList: List<String>, val context: Context) :
+        RecyclerView.Adapter<DevicesRecyclerViewAdapter.VH>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): VH? {
-
+        val view = LayoutInflater.from(context).inflate(R.layout.recyclerview_single_item, parent, false)
+        return VH(view)
     }
 
-    override fun onBindViewHolder(holder: ?, position: Int) {
+    override fun onBindViewHolder(holder: VH?, position: Int) {
     }
 
     override fun getItemCount(): Int {
+        return mList.size
     }
 
-    class VH(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    class VH(itemView: View?) : RecyclerView.ViewHolder(itemView){
 
-
-    }
-
+    val label = itemView?.findViewById<TextView>(R.id.largeLabel)
+}
 
 }
