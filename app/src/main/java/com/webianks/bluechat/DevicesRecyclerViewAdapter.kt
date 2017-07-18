d3448a401e3a65a5a1c2b4e45c7d168c00e42694
webianks <webianks@gmail.com>
2017-07-18 23:36:54 +0530

    Added recyclerview adapter skeleton.

---
+++ app/src/main/java/com/webianks/bluechat/DevicesRecyclerViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.webianks.bluechat
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by R Ankit on 18-07-2017.
+ */
+
+class DevicesRecyclerViewAdapter : RecyclerView.Adapter<DevicesRecyclerViewAdapter.VH>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): VH? {
+
+    }
+
+    override fun onBindViewHolder(holder: ?, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    class VH(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+
+    }
+
+
+}
