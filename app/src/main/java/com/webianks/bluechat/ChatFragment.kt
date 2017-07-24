11dfdcb520efc50c39d81867f1e214073c4a15cd
webianks <webianks@gmail.com>
2017-07-24 19:26:19 +0530

    Toggling send button.

--- app/src/main/java/com/webianks/bluechat/ChatFragment.kt
+++ app/src/main/java/com/webianks/bluechat/ChatFragment.kt
@@ -2,15 +2,22 @@ package com.webianks.bluechat
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.text.Editable
+import android.widget.ImageView
+
 
 /**
  * Created by ramankit on 24/7/17.
  */
 
 class ChatFragment : Fragment() {
+
+    private lateinit var chatInput: EditText
 
     companion object {
 
@@ -36,7 +43,24 @@ class ChatFragment : Fragment() {
 
     private fun initViews(mView: View) {
 
+        chatInput = mView.findViewById(R.id.chatInput)
+        val chatIcon: ImageView = mView.findViewById(R.id.sendIcon)
+
+        chatInput.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable) {
+
+                if (s.isNotEmpty())
+                    chatIcon.setImageDrawable(activity.getDrawable(R.drawable.ic_send))
+                else
+                    chatIcon.setImageDrawable(activity.getDrawable(R.drawable.ic_send_depri))
+
+            }
+        })
+
     }
+
 
 
 }
